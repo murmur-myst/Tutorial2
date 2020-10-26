9a6328c325ab182eaa1d82b19da8d44410df121a
murmur-myst <70252429+murmur-myst@users.noreply.github.com>
2020-10-25 20:01:10 -0400

    Add files via upload

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    public GameObject player;
+
     private Rigidbody2D rd2d;
 
     public float speed;
@@ -13,22 +15,96 @@ public class PlayerScript : MonoBehaviour
 
     private int scoreValue = 0;
 
+    public Text wintext;
+
+    public Text livestext;
+
+    private int livesValue = 0;
+    private int check;
+    private int check2;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    private SpriteRenderer mySpriteRenderer;
+
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        // Start position of player object 
+        Vector3 start = new Vector3(-3.69f,3.6f,0);
+        player.transform.position = start;
+        score.text = "Score: " + scoreValue.ToString();
+        wintext.text = "";
+        livesValue = 3;
+        livestext.text = "Lives: " + livesValue.ToString();
+        check = 0;
+        check2 = 0;
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
+    void Update(){
+         if (Input.GetKeyDown(KeyCode.A)){
+                anim.SetInteger("State", 1);
+                mySpriteRenderer.flipX = true;
+            }
+            if (Input.GetKeyDown(KeyCode.D)){
+                anim.SetInteger("State", 1);
+                mySpriteRenderer.flipX = false;
+            }
+            if (Input.GetKeyUp(KeyCode.A)){
+                 anim.SetInteger("State", 0);
+                 mySpriteRenderer.flipX = true;
+            }
+            if (Input.GetKeyUp(KeyCode.D)){
+                 anim.SetInteger("State", 0);
+                 mySpriteRenderer.flipX = false;
+            }
+            if (Input.GetKeyDown(KeyCode.W)){
+                anim.SetInteger("State", 2);
+            }
+            if (Input.GetKeyUp(KeyCode.W)){
+                anim.SetInteger("State", 0);
+            }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        if(livesValue > 0){
+            float hozMovement = Input.GetAxis("Horizontal");
+            float vertMovement = Input.GetAxis("Vertical");
+            rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+           
+        }
         if (Input.GetKey("escape"))
         {
             Application.Quit();
+        }
+        if (scoreValue == 4 && check == 0){
+            Vector3 stage2 = new Vector3(63.06f,2.14f,0);
+            player.transform.position = stage2;
+            check = 1;
+            livesValue = 3;
+            livestext.text = "Lives: " + livesValue.ToString();
+        }
+        if (scoreValue == 8 && check2 == 0){
+            wintext.text = "You win! Game created by Jonathan Murray";
+            musicSource.loop = false;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            check2 = 1;
+        }       
+        if (livesValue == 0){
+            wintext.text = "Your lives have reached 0, Game Over...";
         }
     }
 
@@ -37,7 +113,13 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livestext.text = "Lives: " + livesValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
@@ -47,8 +129,9 @@ public class PlayerScript : MonoBehaviour
     {
         if (collision.collider.tag == "Ground")
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && livesValue > 0)
             {
+                
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
